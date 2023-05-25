91422d4649add62e6fcd707ad3b2130e186b138d
calvinliaci <liacicalvin@gmail.com>
2023-05-25 22:47:24 +0200

    Sponsors in database

--- Project Weerstand Koersel/Model/Sponsor.cs
+++ Project Weerstand Koersel/Model/Sponsor.cs
@@ -8,6 +8,19 @@ namespace Project_Weerstand_Koersel.Model
 {
     public class Sponsor
     {
-        public string SponsorImage { get; set; }
+        public string SponsorImage1 { get; set; }
+        public string SponsorImage2 { get; set; }
+        public string SponsorImage3 { get; set; }
+        public string SponsorImage4 { get; set; }
+        public string SponsorImage5 { get; set; }
+        public string SponsorImage6 { get; set; }
+        public string SponsorImage7 { get; set; }
+        public string SponsorImage8 { get; set; }
+        public string SponsorImage9 { get; set; }
+        public string SponsorImage10 { get; set; }
+        public string SponsorImage11 { get; set; }
+        public string SponsorImage12 { get; set; }
+        public string SponsorImage13 { get; set; }
+        public string HomeImage { get; set; }
     }
 }
--- Project Weerstand Koersel/Service/SponsorService.cs
+++ Project Weerstand Koersel/Service/SponsorService.cs
@@ -16,7 +16,20 @@ namespace Project_Weerstand_Koersel.Service
         {
             return (await firebaseClient.Child("Sponsor").OnceAsync<Sponsor>()).Select(item => new Sponsor
             {
-                SponsorImage = item.Object.SponsorImage,
+                SponsorImage1 = item.Object.SponsorImage1,
+                SponsorImage2 = item.Object.SponsorImage2,
+                SponsorImage3 = item.Object.SponsorImage3,
+                SponsorImage4 = item.Object.SponsorImage4,
+                SponsorImage5 = item.Object.SponsorImage5,
+                SponsorImage6 = item.Object.SponsorImage6,
+                SponsorImage7 = item.Object.SponsorImage7,
+                SponsorImage8 = item.Object.SponsorImage8,
+                SponsorImage9 = item.Object.SponsorImage9,
+                SponsorImage10 = item.Object.SponsorImage10,
+                SponsorImage11 = item.Object.SponsorImage11,
+                SponsorImage12 = item.Object.SponsorImage12,
+                SponsorImage13 = item.Object.SponsorImage13,
+                HomeImage = item.Object.HomeImage,
 
             }).ToList();
         }
